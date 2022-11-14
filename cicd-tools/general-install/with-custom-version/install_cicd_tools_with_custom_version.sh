8defd7eeab2678a3b3250781beee3d59998abd21
GabrielUC <gabrielurias100@gmail.com>
2022-11-14 08:49:08 -0600

    remove kuebeadme custom version

--- cicd-tools/general-install/with-custom-version/install_cicd_tools_with_custom_version.sh
+++ cicd-tools/general-install/with-custom-version/install_cicd_tools_with_custom_version.sh
@@ -58,22 +58,6 @@ sudo mv ./rke_linux-amd64 /usr/local/bin/rke
 
 rke --version
 
-echo "----------------- Kubeadm -----------------"
-
-sudo apt-get update
-
-sudo apt-get install -y apt-transport-https ca-certificates curl
-
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-sudo apt-get update
-
-sudo apt-get install -y kubelet kubeadm kubectl
-
-sudo apt-mark hold kubelet kubeadm kubectl
-
 echo "----------------- SSH -----------------"
 
 
