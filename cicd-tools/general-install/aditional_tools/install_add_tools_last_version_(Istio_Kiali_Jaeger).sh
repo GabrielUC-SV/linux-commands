7a5006ee8899cdc63d87d3fee58efa8f12f09212
GabrielUC <gabrielurias100@gmail.com>
2022-11-17 09:30:29 -0600

    add docker file

--- cicd-tools/general-install/aditional_tools/install_add_tools_last_version_(Istio_Kiali_Jaeger).sh
+++ cicd-tools/general-install/aditional_tools/install_add_tools_last_version_(Istio_Kiali_Jaeger).sh
@@ -103,4 +103,14 @@ read -p "Enter yaml file name: " fileJaeger
 
 kubectl apply -f $fileJaeger
 
+echo "----------------- Jenkins -----------------"
+
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | sudo tee \/usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \https://pkg.jenkins.io/debian binary/ | sudo tee \/etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install jenkins
+
 echo "All tools has been installed ........ "
